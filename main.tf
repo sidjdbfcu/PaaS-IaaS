c01c200c0ae1046ffe23199f2962ba384c890783
sidjdbfcu <164883478+sidjdbfcu@users.noreply.github.com>
2025-11-26 18:40:49 +0300

    commit

--- main.tf
+++ main.tf
@@ -34,19 +34,16 @@ resource "vkcs_networking_subnet" "leskina_db_subnet" {
   enable_dhcp = true
 }
 
-# Внешняя сеть
 data "vkcs_networking_network" "extnet" {
   name = "ext-net"
 }
 
-# ОДИН роутер для обеих сетей
 resource "vkcs_networking_router" "app_router" {
   name                = "app-router-leskina"
   admin_state_up      = true
   external_network_id = data.vkcs_networking_network.extnet.id
 }
 
-# Подключаем обе подсети к одному роутеру
 resource "vkcs_networking_router_interface" "app_router_interface" {
   router_id = vkcs_networking_router.app_router.id
   subnet_id = vkcs_networking_subnet.app_subnet.id
@@ -70,7 +67,6 @@ resource "vkcs_networking_secgroup" "db_sg" {
   name = "db-sg-leskina"
 }
 
-# Правила для HAProxy
 resource "vkcs_networking_secgroup_rule" "haproxy_http" {
   direction         = "ingress"
   security_group_id = vkcs_networking_secgroup.haproxy_sg.id
@@ -98,7 +94,6 @@ resource "vkcs_networking_secgroup_rule" "haproxy_ssh" {
   remote_ip_prefix  = "0.0.0.0/0"
 }
 
-# Правила для app-серверов
 resource "vkcs_networking_secgroup_rule" "app_ssh" {
   direction         = "ingress"
   security_group_id = vkcs_networking_secgroup.app_sg.id
@@ -117,7 +112,6 @@ resource "vkcs_networking_secgroup_rule" "app_from_haproxy" {
   port_range_max    = 5000
 }
 
-# Правила для БД - ТОЛЬКО от app-серверов
 resource "vkcs_networking_secgroup_rule" "db_mysql" {
   direction         = "ingress"
   security_group_id = vkcs_networking_secgroup.db_sg.id
@@ -127,7 +121,6 @@ resource "vkcs_networking_secgroup_rule" "db_mysql" {
   port_range_max    = 3306
 }
 
-# ЗАРАНЕЕ СОЗДАЕМ FLOATING IP ДЛЯ ГАРАНТИРОВАННОГО ПОДКЛЮЧЕНИЯ
 resource "vkcs_networking_floatingip" "haproxy_fip" {
   pool = data.vkcs_networking_network.extnet.name
 }
@@ -140,7 +133,6 @@ resource "vkcs_networking_floatingip" "app2_fip" {
   pool = data.vkcs_networking_network.extnet.name
 }
 
-# ВМ HAProxy
 resource "vkcs_compute_instance" "haproxy" {
   name              = "haproxy-leskina"
   image_id          = data.vkcs_images_image.compute.id
@@ -162,7 +154,6 @@ resource "vkcs_compute_instance" "haproxy" {
     fixed_ip_v4 = "192.168.100.10"
   }
 
-  # ДОБАВЛЯЕМ USER_DATA ДЛЯ ГАРАНТИРОВАННОЙ НАСТРОЙКИ СЕТИ
   user_data = <<-EOF
     #cloud-config
     bootcmd:
@@ -175,7 +166,6 @@ resource "vkcs_compute_instance" "haproxy" {
   depends_on = [vkcs_networking_router_interface.app_router_interface]
 }
 
-# ВМ App серверы
 resource "vkcs_compute_instance" "app_servers" {
   count             = 2
   name              = "app-server-${count.index + 1}-leskina"
@@ -198,7 +188,6 @@ resource "vkcs_compute_instance" "app_servers" {
     fixed_ip_v4 = "192.168.100.${20 + count.index}"
   }
 
-  # USER_DATA ДЛЯ ГАРАНТИРОВАННОЙ НАСТРОЙКИ СЕТИ
   user_data = <<-EOF
     #cloud-config
     bootcmd:
@@ -211,7 +200,6 @@ resource "vkcs_compute_instance" "app_servers" {
   depends_on = [vkcs_networking_router_interface.app_router_interface]
 }
 
-# ПРИВЯЗЫВАЕМ FLOATING IP К ВМ
 resource "vkcs_compute_floatingip_associate" "haproxy_fip_assoc" {
   floating_ip = vkcs_networking_floatingip.haproxy_fip.address
   instance_id = vkcs_compute_instance.haproxy.id
@@ -227,7 +215,6 @@ resource "vkcs_compute_floatingip_associate" "app2_fip_assoc" {
   instance_id = vkcs_compute_instance.app_servers[1].id
 }
 
-# MANAGED MYSQL DATABASE
 data "vkcs_compute_flavor" "db" {
   name = "STD3-2-8" 
 }
@@ -267,7 +254,6 @@ resource "vkcs_db_database" "leskina_test_db" {
   depends_on = [time_sleep.wait_for_mysql]
 }
 
-# Создаем пользователя БД
 resource "vkcs_db_user" "app_user" {
   name        = "appuser"
   password    = var.db_password
@@ -276,13 +262,11 @@ resource "vkcs_db_user" "app_user" {
   depends_on = [time_sleep.wait_for_mysql]
 }
 
-# SSH ключ
 resource "vkcs_compute_keypair" "ssh_key" {
   name       = "prod-key-leskina"
   public_key = ("****")
 }
 
-# Outputs
 output "haproxy_public_ip" {
   value = vkcs_networking_floatingip.haproxy_fip.address
 }
@@ -318,4 +302,5 @@ output "database_config" {
     instance_class = "STD3-2-8"
   }
   sensitive = true
+
 }
