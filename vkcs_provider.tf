c312ca665d9ac5f766fbddbd3850479b310dfb75
sidjdbfcu <164883478+sidjdbfcu@users.noreply.github.com>
2025-11-26 18:45:26 +0300

    commit

--- vkcs_provider.tf
+++ vkcs_provider.tf
@@ -12,13 +12,14 @@ provider "vkcs" {
     username = "LeskinaSM22@st.ithub.ru"
 
     # The password of the account
-    password = "400160MKnU8AVJ/"
+    password = "****"
 
-    project_id = "259eb7c6129a46f6b01062f53d57b9f8"
+    project_id = "****"
 
     # Region name
     region = "RegionOne"
 
     auth_url = "https://infra.mail.ru:35357/v3/"
+
 
 }
